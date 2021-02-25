24cd133a5ad2f440368a61bf82cdcbbf8d417a1f
Taarnborg <ronnie@ogtal.dk>
2021-02-25 15:40:53 +0100

    to electra

--- text_classifier/utils.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-def remove_invalid_inputs(dataset,text_column):
-    dataset['valid'] = dataset[text_column].apply(lambda x: isinstance(x, str))
-    return dataset.loc[dataset.valid]
